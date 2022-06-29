46664f6220fc4699c0e4d3483f3a5855b23c3982
Elikill58 <arpetzouille@gmail.com>
2022-06-29 19:28:56 +0200

    Add 1.19 support

---
+++ spigot/spigot19/src/com/elikill58/negativity/spigot19/Spigot_1_19_R1.java
@@ -0,0 +1,33 @@
+package com.elikill58.negativity.spigot19;
+
+import org.bukkit.craftbukkit.v1_19_R1.block.CraftBlock;
+import org.bukkit.craftbukkit.v1_19_R1.inventory.CraftItemStack;
+import org.bukkit.entity.Player;
+
+import com.elikill58.ultimatehammer.api.block.Block;
+import com.elikill58.ultimatehammer.api.item.ItemStack;
+import com.elikill58.ultimatehammer.spigot.nms.SpigotVersionAdapter;
+import com.elikill58.ultimatehammer.spigot.utils.PacketUtils;
+
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class Spigot_1_19_R1 extends SpigotVersionAdapter {
+
+	public Spigot_1_19_R1() {
+		super("v1_19_R1");
+	}
+	
+	@Override
+	public int getXpToDrop(Block b, int bonusLevel, ItemStack item) {
+		CraftBlock obcBlock = (CraftBlock) b.getDefault();
+		BlockState blockData = obcBlock.getNMS();
+		return blockData.getBlock().getExpDrop(blockData, obcBlock.getCraftWorld().getHandle(), obcBlock.getPosition(), CraftItemStack.asNMSCopy((org.bukkit.inventory.ItemStack) item.getDefault()), false);
+	}
+	
+	@Override
+	public void sendPacket(Player p, Object packet) {
+		((ServerPlayer) PacketUtils.getEntityPlayer(p)).connection.send((Packet<?>) packet);
+	}
+}
