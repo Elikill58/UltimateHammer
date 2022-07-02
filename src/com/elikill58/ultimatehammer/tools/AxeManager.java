81b4dce34f5570a5eb74e8b79c7b354e2e8f2c36
Elikill58 <arpetzouille@gmail.com>
2022-07-02 16:15:19 +0200

    Fix axe item

--- src/com/elikill58/ultimatehammer/tools/AxeManager.java
+++ src/com/elikill58/ultimatehammer/tools/AxeManager.java
@@ -1,22 +1,23 @@
 package com.elikill58.ultimatehammer.tools;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.LeavesDecayEvent;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
 
 import com.elikill58.ultimatehammer.UltimateHammer;
 import com.elikill58.ultimatehammer.UltimateTool;
@@ -24,165 +25,133 @@ import com.elikill58.ultimatehammer.WorldRegionBypass;
 import com.elikill58.ultimatehammer.utils.Utils;
 
 public class AxeManager extends UltimateTool implements Listener {
-	
+
+	private static final int RANGE = 4;
+	public static final List<Vector> CHECKING_VECTORS = Arrays.asList(new Vector(0, 1, 0), new Vector(0, -1, 0),
+			new Vector(1, 0, 0), new Vector(0, 0, 1), new Vector(-1, 0, 0), new Vector(0, 0, -1),
+			new Vector(1, 1, 0), new Vector(0, 1, 1), new Vector(-1, 1, 0), new Vector(0, 1, -1),
+			new Vector(1, -1, 0), new Vector(0, -1, 1), new Vector(-1, -1, 0), new Vector(0, -1, -1));
+
 	public AxeManager(UltimateHammer pl) {
 		super(pl, "axe");
 	}
-	
+
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onBlockBreak(BlockBreakEvent e) {
-		if(!isEnabled() || e.isCancelled())
+		if (!isEnabled() || e.isCancelled())
 			return;
-		if(!isItem(Utils.getItemInHand(e.getPlayer())))
+		if (!isItem(Utils.getItemInHand(e.getPlayer())))
 			return;
-		if(WorldRegionBypass.cannotBuild(e.getPlayer(), this, e.getBlock().getLocation()))
+		Block b = e.getBlock();
+		if (WorldRegionBypass.cannotBuild(e.getPlayer(), this, b.getLocation()))
 			return;
-		logDetele(e);
+		if (b.getType().name().contains("LOG"))
+			logDetele(e);
 	}
 
-	
 	private void logDetele(BlockBreakEvent e) {
+		Block originBlock = e.getBlock();
+		Material originType = originBlock.getType();
 		Player p = e.getPlayer();
-        p.giveExp(10);
-        List<Block> blist = new ArrayList<>();
-        checkLeaves(p, e.getBlock());
-        blist.add(e.getBlock());
+		p.giveExp(10);
+		List<Block> blist = new ArrayList<>();
+		checkLeaves(p, originBlock, RANGE);
+		blist.add(originBlock);
+		AtomicInteger range = new AtomicInteger(RANGE);
+		new BukkitRunnable() {
+			@Override
+			public void run() {
+				for (int i = 0; i < blist.size(); i++) {
+					Block b = blist.get(i);
+					if (b.getType().equals(originType)) {
+						b.breakNaturally(Utils.getItemInHand(p));
+						checkLeaves(p, b, range.get());
+					}
+					for (Vector v : new ArrayList<>(CHECKING_VECTORS)) {
+						Block faceBlock = b.getRelative(v.getBlockX(), v.getBlockY(), v.getBlockZ());
+						if (faceBlock.getType().equals(originType))
+							blist.add(faceBlock);
+					}
 
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < blist.size(); i++) {
-                    Block b = blist.get(i);
-                    if (b.getType().name().contains("LOG")) {
-                        for (ItemStack item : b.getDrops())
-                            b.getWorld().dropItemNaturally(b.getLocation(), item);
+					blist.remove(b);
 
-                        b.setType(Material.AIR);
-                        checkLeaves(p, b);
-                    }
-                    for (BlockFace face : BlockFace.values()) {
-                        if (b.getRelative(face).getType().name().contains("LOG"))
-                            blist.add(b.getRelative(face));
-                    }
+					if (blist.size() == 0) {
+						cancel();
+					}
+				}
+				range.decrementAndGet();
+				if(range.get() <= 0)
+					range.set(1); // keep at least 1 of range
+			}
+		}.runTaskTimer(getPlugin(), 0, 1);
+	}
 
-                    blist.remove(b);
+	private void checkLeaves(Player p, Block block, int range) {
+		Location loc = block.getLocation();
+		final World world = loc.getWorld();
+		final int x = loc.getBlockX(), y = loc.getBlockY(), z = loc.getBlockZ();
+		final int off = range;
 
-                    if (blist.size() == 0) {
-                        cancel();
-                    }
-                }
-            }
-        }.runTaskTimer(getPlugin(), 0, 1);
-    }
+		if (!validChunk(world, x - off, y - off, z - off, x + off, y + off, z + off))
+			return;
 
-    @SuppressWarnings("deprecation")
-    private void breakLeaf(Player p, World world, int x, int y, int z) {
-        Block block = world.getBlockAt(x, y, z);
-        byte data = block.getData();
+		Material leavesType = null;
+		for (int offX = -range; offX <= range; offX++) {
+			for (int offY = -range - 1; offY <= range + 1; offY++) { // make the Y move easier (instead of X/Z one
+				for (int offZ = -range; offZ <= range; offZ++) {
+					Block offBlock = world.getBlockAt(x + offX, y + offY, z + offZ);
+					if (offBlock.getType().name().contains("LEAVES")) {
+						if (leavesType == null || offBlock.getType().equals(leavesType)) {
+							leavesType = offBlock.getType();
+							breakLeaf(p, offBlock);
+						}
+					}
+				}
+			}
+		}
+	}
 
-        if ((data & 4) == 4) {
-            return;
-        }
+	private void breakLeaf(Player p, Block block) {
+		World world = p.getWorld();
+		int x = block.getX(), y = block.getY(), z = block.getZ();
 
-        byte range = 4;
-        byte max = 32;
-        int[] blocks = new int[max * max * max];
-        int off = range + 1;
-        int mul = max * max;
-        int div = max / 2;
+		int off = RANGE;
 
-        if (validChunk(world, x - off, y - off, z - off, x + off, y + off, z + off)) {
-            int offX;
-            int offY;
-            int offZ;
-            int type;
+		if (validChunk(world, x - off, y - off, z - off, x + off, y + off, z + off)) {
+			for (int offX = -RANGE; offX <= RANGE; offX++) {
+				for (int offY = -RANGE; offY <= RANGE; offY++) {
+					for (int offZ = -RANGE; offZ <= RANGE; offZ++) {
+						Material mat = world.getBlockAt(x + offX, y + offY, z + offZ).getType();
+						if (mat.name().contains("LOG")) {
+							LeavesDecayEvent event = new LeavesDecayEvent(block);
+							Bukkit.getServer().getPluginManager().callEvent(new LeavesDecayEvent(block));
+							if (event.isCancelled())
+								continue;
+							if (WorldRegionBypass.cannotBuild(p, this, block.getLocation()))
+								continue;
+							block.breakNaturally();
+							return;
+						}
+					}
+				}
+			}
+		}
+	}
 
-            for (offX = -range; offX <= range; offX++) {
-                for (offY = -range; offY <= range; offY++) {
-                    for (offZ = -range; offZ <= range; offZ++) {
-                        Material mat = world.getBlockAt(x + offX, y + offY, z + offZ).getType();
-                        if (mat.name().contains("LEAVES"))
-                            type = mat.getId();
-                        else if (mat.name().contains("LOG"))
-                            type = mat.getId();
-                        blocks[(offX + div) * mul + (offY + div) * max + offZ + div]
-                        		= (mat.name().contains("LOG") ? 0  : (mat.name().contains("LEAVES") ? -2 : -1));
-                    }
-                }
-            }
+	public boolean validChunk(World world, int minX, int minY, int minZ, int maxX, int maxY, int maxZ) {
+		if (maxY >= 0 && minY < world.getMaxHeight()) {
+			minX >>= 4;
+			minZ >>= 4;
+			maxX >>= 4;
+			maxZ >>= 4;
 
-            for (offX = 1; offX <= 4; offX++) {
-                for (offY = -range; offY <= range; offY++) {
-                    for (offZ = -range; offZ <= range; offZ++) {
-                        for (type = -range; type <= range; type++) {
-                            if (blocks[(offY + div) * mul + (offZ + div) * max + type + div] == offX - 1) {
-                                if (blocks[(offY + div - 1) * mul + (offZ + div) * max + type + div] == -2)
-                                    blocks[(offY + div - 1) * mul + (offZ + div) * max + type + div] = offX;
-                                if (blocks[(offY + div + 1) * mul + (offZ + div) * max + type + div] == -2)
-                                    blocks[(offY + div + 1) * mul + (offZ + div) * max + type + div] = offX;
-                                if (blocks[(offY + div) * mul + (offZ + div - 1) * max + type + div] == -2)
-                                    blocks[(offY + div) * mul + (offZ + div - 1) * max + type + div] = offX;
-                                if (blocks[(offY + div) * mul + (offZ + div + 1) * max + type + div] == -2)
-                                    blocks[(offY + div) * mul + (offZ + div + 1) * max + type + div] = offX;
-                                if (blocks[(offY + div) * mul + (offZ + div) * max + (type + div - 1)] == -2)
-                                    blocks[(offY + div) * mul + (offZ + div) * max + (type + div - 1)] = offX;
-                                if (blocks[(offY + div) * mul + (offZ + div) * max + type + div + 1] == -2)
-                                    blocks[(offY + div) * mul + (offZ + div) * max + type + div + 1] = offX;
-                            }
-                        }
-                    }
-                }
-            }
-        }
+			for (int x = minX; x <= maxX; x++)
+				for (int z = minZ; z <= maxZ; z++)
+					if (!world.isChunkLoaded(x, z))
+						return false;
+			return true;
+		}
 
-        if (blocks[div * mul + div * max + div] < 0) {
-            LeavesDecayEvent event = new LeavesDecayEvent(block);
-            Bukkit.getServer().getPluginManager().callEvent(new LeavesDecayEvent(block));
-            if (event.isCancelled())
-                return;
-            if(WorldRegionBypass.cannotBuild(p, this, block.getLocation()))
-            	return;
-            block.breakNaturally();
-        }
-    }
-
-    public boolean validChunk(World world, int minX, int minY, int minZ, int maxX, int maxY, int maxZ) {
-        if (maxY >= 0 && minY < world.getMaxHeight()) {
-            minX >>= 4;
-            minZ >>= 4;
-            maxX >>= 4;
-            maxZ >>= 4;
-
-            for (int x = minX; x <= maxX; x++)
-                for (int z = minZ; z <= maxZ; z++)
-                    if (!world.isChunkLoaded(x, z))
-                        return false;
-            return true;
-        }
-
-        return false;
-    }
-
-    private void checkLeaves(Player p, Block block) {
-        Location loc = block.getLocation();
-        final World world = loc.getWorld();
-        final int x = loc.getBlockX();
-        final int y = loc.getBlockY();
-        final int z = loc.getBlockZ();
-        final int range = 4;
-        final int off = range + 1;
-
-        if (!validChunk(world, x - off, y - off, z - off, x + off, y + off, z + off))
-            return;
-
-        Bukkit.getServer().getScheduler().runTask(getPlugin(), new Runnable() {
-            public void run() {
-                for (int offX = -range; offX <= range; offX++)
-                    for (int offY = -range; offY <= range; offY++)
-                        for (int offZ = -range; offZ <= range; offZ++)
-                            if (world.getBlockAt(x + offX, y + offY, z + offZ).getType().name().contains("LEAVES"))
-                                breakLeaf(p, world, x + offX, y + offY, z + offZ);
-            }
-        });
-    }
+		return false;
+	}
 }
