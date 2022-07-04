7aa554518416a137d899ffdcba64f15e08874d23
Elikill58 <arpetzouille@gmail.com>
2022-07-04 19:49:49 +0200

    Fix FactionsUUID versions

--- src/com/elikill58/ultimatehammer/support/factions/FactionsUUIDSupport.java
+++ src/com/elikill58/ultimatehammer/support/factions/FactionsUUIDSupport.java
@@ -6,12 +6,13 @@ import org.bukkit.entity.Player;
 import com.massivecraft.factions.Board;
 import com.massivecraft.factions.FLocation;
 import com.massivecraft.factions.FPlayers;
-import com.massivecraft.factions.perms.PermissibleAction;
+import com.massivecraft.factions.perms.PermissibleActions;
 
 public class FactionsUUIDSupport implements BuildChecker {
 
 	@Override
 	public boolean canBuild(Player p, Location loc) {
-		return Board.getInstance().getFactionAt(new FLocation(loc)).hasAccess(FPlayers.getInstance().getByPlayer(p), PermissibleAction.BUILD);
+		FLocation floc = new FLocation(loc);
+		return Board.getInstance().getFactionAt(floc).hasAccess(FPlayers.getInstance().getByPlayer(p), PermissibleActions.BUILD, floc);
 	}
 }
