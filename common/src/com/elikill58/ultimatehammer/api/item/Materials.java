49ce2270a8ba23904ed41fa0abc919e5a0f5d9e3
Elikill58 <arpetzouille@gmail.com>
2022-07-04 20:14:12 +0200

    Upgrade hoe

--- common/src/com/elikill58/ultimatehammer/api/item/Materials.java
+++ common/src/com/elikill58/ultimatehammer/api/item/Materials.java
@@ -8,23 +8,28 @@ public class Materials {
     public static final Material BEDROCK = ItemRegistrar.getInstance().get("bedrock");
     public static final Material GRASS = ItemRegistrar.getInstance().get("grass_block", "grass");
     public static final Material DIRT = ItemRegistrar.getInstance().get("dirt");
-    public static final Material SOIL = ItemRegistrar.getInstance().get("soil");
+    public static final Material SOIL = ItemRegistrar.getInstance().get("soil", "farmland");
     public static final Material SOUL_SAND = ItemRegistrar.getInstance().get("soul_sand");
 
-    public static final Material SEED = ItemRegistrar.getInstance().get("seeds", "wheat_seeds");
-    public static final Material CROPS = ItemRegistrar.getInstance().get("crops", "seeds", "wheat_seeds");
+	public static final Material SEEDS = ItemRegistrar.getInstance().get("seeds", "wheat_seeds");
+    public static final Material CROPS = ItemRegistrar.getInstance().get("crops", "wheat");
     
     public static final Material CARROT = ItemRegistrar.getInstance().get("carrot");
-    public static final Material CARROT_ITEM = ItemRegistrar.getInstance().get("carrot_item", "carrot");
+    public static final Material CARROT_ITEM = ItemRegistrar.getInstance().get("carrot_item", "carrots");
     
     public static final Material POTATO = ItemRegistrar.getInstance().get("potato");
-    public static final Material POTATO_ITEM = ItemRegistrar.getInstance().get("potato_item", "potato");
+    public static final Material POTATO_ITEM = ItemRegistrar.getInstance().get("potato_item", "potatoes");
     
     public static final Material NETHER_WART = ItemRegistrar.getInstance().get("nether_warts", "nether_wart");
     public static final Material NETHER_WART_ITEM = ItemRegistrar.getInstance().get("nether_stalk", "nether_wart");
 
+	public static final Material PUMPKIN_SEEDS = ItemRegistrar.getInstance().get("pumpkin_seeds");
+	public static final Material MELON_SEEDS = ItemRegistrar.getInstance().get("melon_seeds");
+
+	public static final Material PUMPKIN = ItemRegistrar.getInstance().get("pumpkin_stem");
+	public static final Material MELON = ItemRegistrar.getInstance().get("melin_stem");
+
     public static final Material PLAYER_HEAD = ItemRegistrar.getInstance().get("player_head", "skull_item", "skull");
-    
     
     /* STAINED MATERIALS */
     
--- common/src/com/elikill58/ultimatehammer/common/tools/HoeManager.java
+++ common/src/com/elikill58/ultimatehammer/common/tools/HoeManager.java
@@ -21,6 +21,7 @@ import com.elikill58.ultimatehammer.common.tools.hoe.Plantable.PlantableType;
 import com.elikill58.ultimatehammer.common.tools.hoe.pickup.LocationActions;
 import com.elikill58.ultimatehammer.universal.Adapter;
 import com.elikill58.ultimatehammer.universal.Scheduler;
+import com.elikill58.ultimatehammer.universal.Version;
 import com.elikill58.ultimatehammer.universal.bypass.WorldRegionBypass;
 
 public class HoeManager extends UltimateToolType implements Listeners {
@@ -29,10 +30,22 @@ public class HoeManager extends UltimateToolType implements Listeners {
 
 	public HoeManager() {
 		super(KEY);
-		add(PlantableType.BASIC, Materials.SEED, Materials.CROPS, (byte) 7);
+		add(PlantableType.BASIC, Materials.SEEDS, Materials.CROPS, (byte) 7);
 		add(PlantableType.BASIC, Materials.POTATO_ITEM, Materials.POTATO, (byte) 7);
 		add(PlantableType.BASIC, Materials.CARROT_ITEM, Materials.CARROT, (byte) 7);
 		add(PlantableType.NETHER, Materials.NETHER_WART_ITEM, Materials.NETHER_WART, (byte) 3);
+
+		Version v = Version.getVersion();
+		add(PlantableType.BASIC, Materials.SEEDS, Materials.CROPS, (byte) 7);
+		add(PlantableType.BASIC, Materials.PUMPKIN_SEEDS, Materials.PUMPKIN, (byte) -1);
+		add(PlantableType.BASIC, Materials.MELON_SEEDS, Materials.MELON, (byte) -1);
+
+		add(PlantableType.BASIC, Materials.POTATO_ITEM, Materials.POTATO, (byte) 7);
+		add(PlantableType.BASIC, Materials.CARROT_ITEM, Materials.CARROT, (byte) 7);
+			
+		add(PlantableType.NETHER, Materials.NETHER_WART_ITEM, Materials.NETHER_WART, (byte) 3);
+		if(v.isNewerOrEquals(Version.V1_9))
+			add(PlantableType.BASIC, Materials.CARROT_ITEM, Materials.CARROT, (byte) 7);
 	}
 	
 	private void add(PlantableType type, Material invItem, Material nextItem, byte neededDataToGet) {
