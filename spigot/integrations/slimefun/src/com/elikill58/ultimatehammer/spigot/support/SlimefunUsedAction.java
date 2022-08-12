a9bf4262a43e26ecc6189b6b62e21d131bb76a65
Elikill58 <arpetzouille@gmail.com>
2022-08-12 11:52:18 +0200

    Add slimefun support (Fix #7)

---
+++ spigot/integrations/slimefun/src/com/elikill58/ultimatehammer/spigot/support/SlimefunUsedAction.java
@@ -0,0 +1,38 @@
+package com.elikill58.ultimatehammer.spigot.support;
+
+import org.bukkit.event.block.BlockBreakEvent;
+
+import com.elikill58.ultimatehammer.api.block.Block;
+import com.elikill58.ultimatehammer.api.entity.Player;
+import com.elikill58.ultimatehammer.api.item.ItemStack;
+import com.elikill58.ultimatehammer.common.UltimateTool;
+import com.elikill58.ultimatehammer.universal.Adapter;
+import com.elikill58.ultimatehammer.universal.PluginDependentExtension;
+import com.elikill58.ultimatehammer.universal.support.UsedActionManager;
+import com.elikill58.ultimatehammer.universal.support.UsedActionManagerProvider;
+
+import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
+import io.github.thebusybiscuit.slimefun4.implementation.items.magical.talismans.Talisman;
+
+public class SlimefunUsedAction implements UsedActionManager {
+
+	@Override
+	public boolean usedBreak(UltimateTool tool, Player p, ItemStack item, Block b) {
+		BlockBreakEvent event = new BlockBreakEvent((org.bukkit.block.Block) b, (org.bukkit.entity.Player) p);
+		Slimefun.getRegistry().getAllSlimefunItems().forEach(sf -> Talisman.trigger(event, sf));
+		return event.isCancelled();
+	}
+	
+	public static class Provider implements UsedActionManagerProvider, PluginDependentExtension {
+		
+		@Override
+		public UsedActionManager create(Adapter adapter) {
+			return new SlimefunUsedAction();
+		}
+		
+		@Override
+		public String getPluginId() {
+			return "Slimefun";
+		}
+	}
+}
